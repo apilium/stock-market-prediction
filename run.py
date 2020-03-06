d61252b3a825f9b02d95b973cc16ae46dc41a0b2
Abdullah Gunay <abdullahgunay96@gmail.com>
2020-03-06 11:46:33 +0300

    Added config and main file

---
+++ run.py
@@ -0,0 +1,72 @@
+import os
+import json
+import time
+import math
+import matplotlib.pyplot as plt
+from core.data_processor import DataLoader
+from core.model import Model
+
+#plotting function for comparison between true data and predicted data
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.title('S&P 500 Stock Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel('Normalized/Standardized Stock Price')
+    plt.legend()
+    plt.show()
+
+def main():
+    configs = json.load(open('config.json', 'r'))
+    if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
+
+    data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+
+    model = Model()
+    model.build_model(configs)
+    x, y = data.get_train_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+    '''
+	# in-memory training (heavier computation)
+	model.train(
+		x,
+		y,
+		epochs = configs['training']['epochs'],
+		batch_size = configs['training']['batch_size'],
+		save_dir = configs['model']['save_dir']
+	)
+	'''
+    # out-of memory generative training
+    steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
+    model.train_generator(
+        data_gen=data.generate_train_batch(
+            seq_len=configs['data']['sequence_length'],
+            batch_size=configs['training']['batch_size'],
+            normalise=configs['data']['normalise']
+        ),
+        epochs=configs['training']['epochs'],
+        batch_size=configs['training']['batch_size'],
+        steps_per_epoch=steps_per_epoch,
+        save_dir=configs['model']['save_dir']
+    )
+
+    x_test, y_test = data.get_test_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+    #predictions are made point by point with model.predict_point_by_point
+    predictions = model.predict_point_by_point(x_test)
+    plot_results(predictions, y_test)
+
+if __name__ == '__main__':
+    main()
